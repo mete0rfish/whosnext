d9011ebc757340d5cdb813527cbd9b9ec295ceda
mete0rfish <sungwon326@naver.com>
2026-01-06 11:25:54 +0900

    chore: CORS 수정

--- backend/src/main/java/com/meteorfish/whosnext/global/config/SecurityConfig.java
+++ backend/src/main/java/com/meteorfish/whosnext/global/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://whosnext-mu.vercel.app/", "whosnext-8h7u1iyiu-sungwons-projects-9cffadc6.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
