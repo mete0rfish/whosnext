7e1aaf045168b31f4d33e519ec01e9ad47934048
mete0rfish <sungwon326@naver.com>
2026-01-08 18:48:10 +0900

    initial commit

---
+++ mcp/src/main/java/com/meteorfish/whosnext_mcp/WhosnextMcpApplication.java
@@ -0,0 +1,13 @@
+package com.meteorfish.whosnext_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhosnextMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhosnextMcpApplication.class, args);
+	}
+
+}
